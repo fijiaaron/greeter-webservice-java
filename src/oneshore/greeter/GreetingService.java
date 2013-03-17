97d6991c44fae64727fe338de9af0dd9783318fc
Aaron Evans <fijiaaron@fast-mail.org>
2013-03-17 07:34:18 -0600

    More person/locale/string shenanigans to make the WSDL generator happy.

--- src/oneshore/greeter/GreetingService.java
+++ src/oneshore/greeter/GreetingService.java
@@ -2,12 +2,36 @@ package oneshore.greeter;
 
 
 public class GreetingService {
-
+	
+	
+	
+	
 	public GreetingService() {}
 	
-	public Greeting greet(Person person, String locale) {
-		Greeter greeter = new Greeter(locale);
+	
+	public Greeting greet() {
+		return greet(null);
+	}
+	
+	public Greeting greet(String name) {
+		return greet(name, null);
+	}
+	
+	public Greeting greet(String name, String locale) {
+		Person person = null;
+		Greeter greeter = new Greeter();
+		
+		if (name != null) {
+			person = new Person(name);
+		}
+		
+		if (locale != null) {
+			greeter.setLocale(locale);
+		}
+		
 		Greeting greeting = greeter.greet(person, locale);
 		return greeting;
 	}
+	
+	
 }
