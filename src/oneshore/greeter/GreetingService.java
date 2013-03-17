0fbbc9f0b2ab9e84d7a517d8fab434eb994b5eea
Aaron Evans <fijiaaron@fast-mail.org>
2013-03-17 07:17:31 -0600

    Corrected to use String for locale

--- src/oneshore/greeter/GreetingService.java
+++ src/oneshore/greeter/GreetingService.java
@@ -5,7 +5,7 @@ public class GreetingService {
 
 	public GreetingService() {}
 	
-	public Greeting greet(Person person, Locales locale) {
+	public Greeting greet(Person person, String locale) {
 		Greeter greeter = new Greeter(locale);
 		Greeting greeting = greeter.greet(person, locale);
 		return greeting;
